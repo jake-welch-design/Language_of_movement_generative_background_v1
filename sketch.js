aafbed32d87dfd4dcb8c17713a760a99a9671f9d
Jake Welch <jaketwelch@gmail.com>
2024-04-17 16:55:29 -0600

    updates

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let graphic;
+let sx,sy,sw,sh,dx,dy,dw,dh;
 
 let img = [];
 let imgAmt = 241;
@@ -8,7 +9,7 @@ let sizeIndex = 0;
 
 let x = 0;
 let y = 0;
-let xSpeed;
+let stepSize;
 
 let size = [50,100,150,200,250,300];
 
@@ -20,6 +21,7 @@ function preload(){
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  frameRate(60);
   background(0);
 }
 
@@ -36,16 +38,19 @@ graphic.translate(graphic.width/2,graphic.height/2);
 graphic.image(img[imgIndex],0,0,imgWidth,imgHeight); 
 graphic.pop();
 
-  xSpeed = size[sizeIndex]; 
-  x += xSpeed;
+  stepSize = size[sizeIndex]; 
 
-  if(x + xSpeed > windowWidth * 1.1){ 
+  if(frameCount % 5 == 0){
+  x += stepSize;
+  }
+
+  if(x + stepSize > windowWidth * 1.1){ 
     x = 0;
     y += size[sizeIndex];
     sizeIndex = floor(random(0,size.length)); 
   }
 
-  if(y + size[sizeIndex] > windowHeight * 1.1){ 
+  if(y + size[sizeIndex] > windowHeight * 1.15){ 
     y = 0;
   }
   image(graphic,x,y,size[sizeIndex],size[sizeIndex]); 
